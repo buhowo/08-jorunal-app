82e2fb1cc364beaf7b12ce5f78720a1be39f48a1
Sergio <buhowoITQ>
2021-08-15 21:19:01 -0500

    googleSignIn method created, btn linked

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,24 +1,51 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { startGoogleLogin, startLoginEmailPassword } from '../../actions/auth';
+import { useForm } from '../../hooks/useForm';
+
+
 
 export const LoginScreen = () => {
+
+  const dispatch = useDispatch();
+
+  const [formValues, handleInputChange,] = useForm({
+    email: 'Sergio@gmail.com',
+    password: '123456',
+  })
+
+  const { email, password } = formValues;
+
+  const handleGoogleLogin = () => {
+    dispatch(startGoogleLogin());
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    dispatch(startLoginEmailPassword(12345, 'Raul'));
+  }
   return (
     <>
 
       <h3 className='auth__title'>Login</h3>
-      <form>
+      <form onSubmit={handleLogin}>
         <input
           type='text'
           placeholder='Email'
           name='email'
           className='auth__input'
           autoComplete='off'
+          value={email}
+          onChange={handleInputChange}
         />
         <input
           type='password'
           placeholder='Password'
           name='password'
           className='auth__input'
+          value={password}
+          onChange={handleInputChange}
         />
         <button className='btn btn-primary btn-block '
           type='submit'
@@ -30,8 +57,11 @@ export const LoginScreen = () => {
           <p>Login with social networks</p>
           <div
             className="google-btn"
+            onClick={handleGoogleLogin}
           >
-            <div className="google-icon-wrapper">
+            <div
+              className="google-icon-wrapper"
+            >
               <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
             </div>
             <p className="btn-text">
